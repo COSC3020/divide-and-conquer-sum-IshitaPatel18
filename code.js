47e6725fe78c3fbf88204bf85876d8e814928cac
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-02-06 15:07:37 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 // Geeks for Geeks was used for inspiration and help
 
-function divideAndConquerSum(x) {
-    return divideAndSum(x, 0, x.length);
+function divideAndConquerSum(a) {
+    return divideAndSum(a, 0, a.length);
 }
 
-function divideAndSum(x, lo, hi)
+function divideAndSum(a, lo, hi)
 { 
     if(hi-lo < 1)
     {
@@ -13,15 +13,15 @@ function divideAndSum(x, lo, hi)
     
     if((hi - lo) === 1)
     {
-        return x[0];
+        return a[0];
     }
 
     let mid1 = lo + Math.floor((hi - lo) / 3);
     let mid2 = lo + 2 * Math.floor((hi - lo) / 3) + 1;
     
-    let part1 = x.slice(0, mid1 + 1);
-    let part2 = x.slice(mid1 + 1, mid2);
-    let part3 = x.slice(mid2, hi);
+    let part1 = a.slice(0, mid1 + 1);
+    let part2 = a.slice(mid1 + 1, mid2);
+    let part3 = a.slice(mid2, hi);
     
     return divideAndSum(part1, 0, part1.length) + divideAndSum(part2, 0, part2.length) + divideAndSum(part3, 0, part3.length);
 }
