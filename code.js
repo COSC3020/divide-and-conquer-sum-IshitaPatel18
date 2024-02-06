fa41f601f4760164614b0879b70412ca4f837f68
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-02-06 15:00:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function divideAndConquerSum(a) {
-    return a;
+    return divideAndSum(x, 0, x.length);
+}
+
+function divideAndSum(x, lo, hi)
+{ 
+    if(hi-lo < 1)
+    {
+        return 0;
+    }
+    
+    if((hi - lo) === 1)
+    {
+        return x[0];
+    }
+
+    let mid1 = lo + Math.floor((hi - lo) / 3);
+    let mid2 = lo + 2 * Math.floor((hi - lo) / 3) + 1;
+    
+    let part1 = x.slice(0, mid1 + 1);
+    let part2 = x.slice(mid1 + 1, mid2);
+    let part3 = x.slice(mid2, hi);
+    
+    return divideAndSum(part1, 0, part1.length) + divideAndSum(part2, 0, part2.length) + divideAndSum(part3, 0, part3.length);
 }
